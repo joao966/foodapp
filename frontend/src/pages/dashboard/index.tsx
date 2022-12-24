9dce0b1917ee22471f1fe84cf7a1543afc70709f
esionascimento <esionascimento@proton.me>
2022-12-23 21:29:20 -0400

    refactor: index dashboard, minimo, resto pro section

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -5,7 +5,9 @@ import { configNavbarTabs } from '@Config/dashboard/index';
 import { Box, Container } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import Grid2 from '@mui/material/Unstable_Grid2';
+import { Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
 import DashboardLayout from 'src/layouts';
+import { SectionInicio } from 'src/section/dashboard/inicio/Home';
 
 PageDashboard.getLayout = function getLayout(page: React.ReactElement) {
   return <DashboardLayout>{page}</DashboardLayout>;
@@ -16,37 +18,29 @@ export default function PageDashboard() {
     <Box>
       <HeadPage title={'FoodNas-Dashboard'} content={'dashboard'} />
       <Container maxWidth={false}>
-        <Box>
-          <ComponentNavbar pages={configNavbarTabs} />
-        </Box>
-        <Box>
-          <Grid2 container spacing={{ mobile: 1, tablet: 2, laptop: 3 }} sx={{ border: '1px solid red' }}>
-            <Grid2 mobile={6} tablet={4} laptop={3}>
-              <Paper>
-                <Box>Entrada</Box>
-                <Box>R$</Box>
-              </Paper>
-            </Grid2>
-            <Grid2 mobile={6} tablet={4} laptop={3}>
-              <Paper>
-                <Box>Saída</Box>
-                <Box>R$</Box>
-              </Paper>
-            </Grid2>
-
-            {/* {Array.from(Array(8)).map((_, index) => (
-              <Grid2 mobile={6} tablet={4} laptop={3} key={index}>
-                <div>{index + 1}</div>
-              </Grid2>
-            ))} */}
+        <Grid2
+          container
+          disableEqualOverflow
+          spacing={{ xs: 1, sm: 2, md: 3 }}
+          sx={{ border: '1px solid red' }}
+        >
+          <Grid2 xs={6} sm={4} md={3}>
+            <Paper>
+              <Box>Entrada</Box>
+              <Box>R$</Box>
+            </Paper>
           </Grid2>
-        </Box>
-        <Box sx={{ bottom: 0, left: 0, position: 'fixed', width: '100%' }}>
-          <Footer />
-        </Box>
+          <Grid2 xs={6} sm={4} md={3}>
+            <Paper>
+              <Box>Saída</Box>
+              <Box>R$</Box>
+            </Paper>
+          </Grid2>
+        </Grid2>
+        <div className="bar">
+          <SectionInicio />
+        </div>
       </Container>
     </Box>
   );
 }
-
-// export default PageDashboard;
