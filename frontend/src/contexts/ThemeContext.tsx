191587f489fbc5c5d55a506b33a64d981c88e0da
esionascimento <esionascimento@proton.me>
2022-12-23 21:26:47 -0400

    feat: ThemeContext

---
+++ frontend/src/contexts/ThemeContext.tsx
@@ -0,0 +1,51 @@
+import { createContext, ReactNode, useState } from 'react';
+
+// ----------------------------------------------------------------------
+
+export const defaultSettings: SettingsValueProps = {
+  themeMode: 'light',
+};
+
+const initialState = {
+  ...defaultSettings,
+  onToggleMode: () => {},
+};
+
+const ThemeContext = createContext(initialState);
+
+type SettingsProviderProps = {
+  children: ReactNode;
+};
+
+export type ThemeMode = 'light' | 'dark';
+
+export type SettingsValueProps = {
+  themeMode: ThemeMode;
+};
+
+function ConfigProvider({ children }: SettingsProviderProps) {
+  const [settings, setSettings] = useState<SettingsValueProps>({
+    themeMode: 'light',
+  });
+  console.log('settings: ', settings);
+
+  const toggleColorMode = () => {
+    setSettings({
+      ...settings,
+      themeMode: settings.themeMode === 'light' ? 'dark' : 'light',
+    });
+  };
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        ...settings,
+        onToggleMode: toggleColorMode,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export { ConfigProvider, ThemeContext };
