e61fd5f3aa95977505ee49a76b02c909799a364b
esionascimento <esionascimento@proton.me>
2022-12-24 19:14:03 -0400

    refactor: pages dashboard, move cards para section

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -7,7 +7,7 @@ import Paper from '@mui/material/Paper';
 import Grid2 from '@mui/material/Unstable_Grid2';
 import { Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
 import DashboardLayout from 'src/layouts';
-import { SectionInicio } from 'src/section/dashboard/inicio/Home';
+import { SectionInicio } from 'src/section/authenticated/dashboard/inicio/Home';
 
 PageDashboard.getLayout = function getLayout(page: React.ReactElement) {
   return <DashboardLayout>{page}</DashboardLayout>;
@@ -18,28 +18,7 @@ export default function PageDashboard() {
     <Box>
       <HeadPage title={'FoodNas-Dashboard'} content={'dashboard'} />
       <Container maxWidth={false}>
-        <Grid2
-          container
-          disableEqualOverflow
-          spacing={{ xs: 1, sm: 2, md: 3 }}
-          sx={{ border: '1px solid red' }}
-        >
-          <Grid2 xs={6} sm={4} md={3}>
-            <Paper>
-              <Box>Entrada</Box>
-              <Box>R$</Box>
-            </Paper>
-          </Grid2>
-          <Grid2 xs={6} sm={4} md={3}>
-            <Paper>
-              <Box>Saída</Box>
-              <Box>R$</Box>
-            </Paper>
-          </Grid2>
-        </Grid2>
-        <div className="bar">
-          <SectionInicio />
-        </div>
+        <SectionInicio />
       </Container>
     </Box>
   );
