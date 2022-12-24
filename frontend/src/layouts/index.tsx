77d151093941b24b41ec91c685fb1363d25340dc
esionascimento <esionascimento@proton.me>
2022-12-23 21:30:20 -0400

    refactor: layouts, add React.Fragment

--- frontend/src/layouts/index.tsx
+++ frontend/src/layouts/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 // @mui
 import { Footer } from '@Components/Footer';
 import { ComponentNavbar } from '@Components/Navbar';
@@ -25,14 +25,17 @@ const MainStyle = styled('main')(({ theme }) => ({
 
 export default function DashboardLayout({ children }: Props) {
   return (
-    <>
+    <React.Fragment>
       <ComponentNavbar pages={configNavbarTabs} />
       <Box>
         <MainStyle>{children}</MainStyle>
       </Box>
-      <Box sx={{ bottom: 0, left: 0, position: 'fixed', width: '100%' }}>
+      {/* <Box sx={{ bottom: 0, left: 0, position: 'fixed', width: '100%' }}>
+        <Footer />
+      </Box> */}
+      <Box sx={{ position: 'absolute', bottom: 0, width: '100%' }}>
         <Footer />
       </Box>
-    </>
+    </React.Fragment>
   );
 }
