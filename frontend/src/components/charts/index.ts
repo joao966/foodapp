15b2f97e1dcb96f8fcbaf4f322c3ec9e11861dba
esionascimento <esionascimento@proton.me>
2022-12-24 19:13:04 -0400

    refactor: components/charts ResponsiveContainer dynamic

--- frontend/src/components/charts/index.ts
+++ frontend/src/components/charts/index.ts
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic';
 
 const AreaChart = dynamic(() => import('recharts/').then((module) => module.AreaChart), { ssr: false });
+const ResponsiveContainer = dynamic(() => import('recharts/').then((module) => module.ResponsiveContainer), { ssr: false });
 
-export default AreaChart;
+export { AreaChart, ResponsiveContainer };
