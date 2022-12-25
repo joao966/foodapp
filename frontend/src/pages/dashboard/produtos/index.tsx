c0e95cc16cfd6afca1aa4e88db6a2a0df93f4a08
esionascimento <esionascimento@proton.me>
2022-12-25 19:26:34 -0400

    feat: page dashboard/produtos

---
+++ frontend/src/pages/dashboard/produtos/index.tsx
@@ -0,0 +1,19 @@
+import { Box, Container } from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2';
+import React from 'react';
+import DashboardLayout from 'src/layouts';
+import { SectionProdutos } from 'src/section/authenticated/dashboard/produtos/SectionProdutos';
+
+PageProdutos.getLayout = function getLayout(page: React.ReactElement) {
+  return <DashboardLayout title={'Produtos'}>{page}</DashboardLayout>;
+};
+
+export default function PageProdutos() {
+  return (
+    <Grid2 container disableEqualOverflow>
+      <Grid2 xs={12}>
+        <SectionProdutos />
+      </Grid2>
+    </Grid2>
+  );
+}
