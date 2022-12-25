7b8e2602331aee6124f14d7eaa1a67c26691fbfa
esionascimento <esionascimento@proton.me>
2022-12-25 19:24:30 -0400

    refactor: config, rename arquivo index->Navbar

--- frontend/src/config/dashboard/Navbar.ts
+++ frontend/src/config/dashboard/Navbar.ts
@@ -1,8 +1,9 @@
 import { PATH_DASHBOARD } from '@Config/paths';
 import { TypesConfigNavbar } from 'src/@types/dashboard';
 
-export const configNavbarTabs: TypesConfigNavbar[] = [
+export const configNavbar: TypesConfigNavbar[] = [
   { text: 'Home', href: `${PATH_DASHBOARD}` },
+  { text: 'Vender', href: `${PATH_DASHBOARD}/venda` },
+  { text: 'Produtos', href: `${PATH_DASHBOARD}/produtos` },
   { text: 'Sobre', href: `${PATH_DASHBOARD}/sobre` },
-  { text: 'Venda', href: `${PATH_DASHBOARD}/venda` },
 ];
