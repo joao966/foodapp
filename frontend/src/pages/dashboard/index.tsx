e6bfa50a922706cb1233ed0283299045f3f0cc41
esionascimento <esionascimento@proton.me>
2022-12-25 19:27:53 -0400

    refactor: pages inicio, clean code

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,16 +1,11 @@
-import { Footer } from '@Components/Footer';
 import { HeadPage } from '@Components/Head';
-import { ComponentNavbar } from '@Components/Navbar';
-import { configNavbarTabs } from '@Config/dashboard/index';
 import { Box, Container } from '@mui/material';
-import Paper from '@mui/material/Paper';
 import Grid2 from '@mui/material/Unstable_Grid2';
-import { Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
 import DashboardLayout from 'src/layouts';
 import { SectionInicio } from 'src/section/authenticated/dashboard/inicio/SectionInicio';
 
 PageDashboard.getLayout = function getLayout(page: React.ReactElement) {
-  return <DashboardLayout>{page}</DashboardLayout>;
+  return <DashboardLayout title={'Início'}>{page}</DashboardLayout>;
 };
 
 export default function PageDashboard() {
